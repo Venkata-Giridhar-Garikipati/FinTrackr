e55b4cdd9edb649ebdb27a7d94d654635e8d9c20
Venkata-Giridhar-Garikipati <venkatagiridhargarikipati@gmail.com>
2025-01-16 15:35:28 +0530

    Update notificationService.js

--- frontend/src/services/notificationService.js
+++ frontend/src/services/notificationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/notifications';
+const API_URL = 'https://fintrackr-backend-l72z.onrender.com/api/notifications';
 
 
 export const getNotifications = async () => {
