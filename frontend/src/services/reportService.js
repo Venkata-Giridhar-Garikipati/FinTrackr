53b7e53d87ef829c50a127a1fa670ed3e6ec1838
Venkata-Giridhar-Garikipati <venkatagiridhargarikipati@gmail.com>
2025-01-16 15:35:43 +0530

    Update reportService.js

--- frontend/src/services/reportService.js
+++ frontend/src/services/reportService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/loans';
+const API_URL = 'https://fintrackr-backend-l72z.onrender.com/api/loans';
 
 
 export const getReport = async () => {
