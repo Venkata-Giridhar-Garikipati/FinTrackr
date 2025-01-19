6004c729e78cc5078bc51b62902d90a77ec97f73
Venkata-Giridhar-Garikipati <venkatagiridhargarikipati@gmail.com>
2025-01-19 14:23:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://fintrackr-ctc8.onrender.com',  // React app's domain
+}));
+
+//app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
